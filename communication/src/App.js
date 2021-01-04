bbd865d34d12a55eec112ca7f5147a6d203ffd9a
Cervantes Torres Ulises Emmanuel (RBEI/EBS3.2-MX) <ceu1ga@bosch.com>
2021-01-04 01:35:52 -0600

    first icons

--- communication/src/App.js
+++ communication/src/App.js
@@ -1,13 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {AiFillCloseCircle, AiFillCheckCircle} from 'react-icons/ai'
+import {IoWater, IoFastFood} from 'react-icons/io5'
+import {GiNightSleep} from 'react-icons/gi'
+import {FaPoo} from 'react-icons/fa'
+
+
+
 
 function App() {
+
+  const hablar = (texto) =>{
+
+    const utterance = new SpeechSynthesisUtterance(texto)
+    utterance.rate = 1;
+    utterance.lang = 'es-MX';
+    speechSynthesis.speak(utterance)
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Ulises</p>
-      </header>
+      <div className='element' > <AiFillCheckCircle onClick={ () => hablar('Si') }/> </div>
+      <div className='element' > <AiFillCloseCircle onClick={ () => hablar('No') }/> </div>
+
+      <div className='element' > <GiNightSleep onClick={ () => hablar('Tengo sueño') }/> </div>
+      <div className='element' > <IoFastFood onClick={ () => hablar('Quiero Comer') } /> </div>
+
+      <div className='element' > <IoWater onClick={ () => hablar('Quiero Aagua') }/> </div>
+      <div className='element' > <FaPoo onClick={ () => hablar('Cámbiame el calzón porfis') }/> </div>
+
+
     </div>
   );
 }
